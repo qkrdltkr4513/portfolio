8983062e16c264fa80036dbf6e31f57a8e952bb6
Austin.park <34262987+qkrdltkr4513@users.noreply.github.com>
2023-04-09 12:29:09 +0900

    [WORK-1] 공용 생성 및 정의 (#2)
    [WORK-1] 컴포넌트 생성
    
    - 프로젝트 내에서 사용 할 공용 컴포넌트 생성 (card, container, header)
    - index 구조 수정 및 _app 수정
    - 프로젝트 내에서 공용으로 사용 될 메뉴 컴포넌트 생성

---
+++ src/components/common/Container.tsx
@@ -0,0 +1,28 @@
+import { css } from '@emotion/react';
+import { themes } from '@styles/themes';
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const wrapperStyle = () => css`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  background-color: ${themes.color['Black/90']};
+`;
+
+const innerStyle = () => css`
+  padding: 20px 40px;
+`;
+
+const Container = ({ children }: ContainerProps) => {
+  return (
+    <div css={wrapperStyle()}>
+      <div css={innerStyle()}>{children}</div>
+    </div>
+  );
+};
+
+export default Container;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from 'next/app';
+import Container from '@components/common/Container';
+import Header from '@components/common/Header';
 import { GlobalStyles } from '@styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <Container>
+        <Header />
+        {/* TODO: script 컴포넌트를 통하여 GA 추가 */}
+        <Component {...pageProps} />
+      </Container>
     </>
   );
 }
