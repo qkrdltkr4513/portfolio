2f8f6f819cd075edf9105b3e07d67b023e800f05
austin <qkrdltkr4513@gmail.com>
2023-04-09 12:26:40 +0900

    [WORK-1] 공용 메뉴 컴포넌트 생성
    - 프로젝트 내에서 공용으로 사용 될 메뉴 컴포넌트 생성

---
+++ src/components/common/MenuList.tsx
@@ -0,0 +1,58 @@
+import { useMemo } from 'react';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import { uniqueId } from 'lodash';
+import { CONSTANTS_MENU_LIST } from '@core/constants';
+import { GlobalMenuListProps } from '@core/types';
+
+import { themes } from '@styles/themes';
+
+const wrapperStyle = () => css`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  gap: 0 50px;
+`;
+
+const itemStyle = (isActive: boolean) => css`
+  display: block;
+  color: ${isActive ? themes.color['Yellow'] : themes.color.White};
+  ${themes.fontSize.Body3};
+  ${themes.fontWeight.Medium};
+  cursor: pointer;
+`;
+
+const MenuList = () => {
+  const { push, pathname } = useRouter();
+
+  const currentMenu = useMemo(
+    () => (pathname !== '/' ? pathname.replace('/', '') : pathname),
+    [pathname],
+  );
+
+  const onClickMenuItem = (item: GlobalMenuListProps) => {
+    const { path } = item;
+    push(path);
+  };
+
+  return (
+    <div css={wrapperStyle()}>
+      {CONSTANTS_MENU_LIST.map((item) => {
+        const { name, path } = item;
+        const isActive = currentMenu === path;
+        return (
+          <span
+            css={itemStyle(isActive)}
+            key={`menu-item-${uniqueId()}`}
+            onClick={() => onClickMenuItem(item)}
+          >
+            {name}
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MenuList;
