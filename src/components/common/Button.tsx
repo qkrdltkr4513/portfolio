3a61a1f26fca128ac711ad1ab542d1bba11a47c6
austin <qkrdltkr4513@gmail.com>
2023-04-15 11:09:08 +0900

    [WORK-2] 버튼 컴포넌트 생성
    - 버튼 컴포넌트 생성 및 스타일 반영
    - 버튼 유형 상수와 타입 추가

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,46 @@
+import { css } from '@emotion/react';
+import { COMMON_BUTTON_TYPES } from '@src/core/constants';
+import { CommonButtonProps, CommonButtonType } from '@src/core/types';
+import { themes } from '@styles/themes';
+
+const wrapperStyle = (type: CommonButtonType) => {
+  const isBlack = type === COMMON_BUTTON_TYPES.BLACK;
+  return css`
+    border-radius: 20px;
+    border: 1px solid ${themes.colors.White};
+    background-color: ${isBlack ? themes.colors['Gray/13'] : themes.colors.White};
+    cursor: pointer;
+  `;
+};
+
+const innerStyle = () => css`
+  padding: 10px 20px;
+`;
+
+const textStyle = (type: CommonButtonType) => {
+  const isBlack = type === COMMON_BUTTON_TYPES.BLACK;
+  return css`
+    display: block;
+    text-align: center;
+    color: ${isBlack ? themes.colors.White : themes.colors['Black/08']};
+    ${themes.fontSize.Body2};
+    ${themes.fontWeight.Medium};
+  `;
+};
+
+const Button = ({
+  type = COMMON_BUTTON_TYPES.BLACK,
+  className,
+  text,
+  onClick,
+}: CommonButtonProps) => {
+  return (
+    <div css={wrapperStyle(type)} onClick={onClick} className={className}>
+      <div css={innerStyle()}>
+        <span css={textStyle(type)}>{text}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Button;
