d1f9dfade97fa5e576335435df18391c077314e7
Austin.park <34262987+qkrdltkr4513@users.noreply.github.com>
2023-05-14 12:09:12 +0900

    [WORK-9][컴포넌트 생성] Article 컴포넌트 생성 (#10)
    [WORK-9][컴포넌트 생성] Article 컴포넌트 생성
    
    - 공용 Title, Contenet 컴포넌트를 통한 Article 컴포넌트 생성
    - Article 컴포넌트 테스트를 위한 컴포넌트 반영 추가
    - menuList 폰트 사이즈 수정

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 import Card from '@components/common/Card';
+import Article from '@components/Article';
 
 const wrapperStyle = () => css`
   display: flex;
@@ -9,15 +10,26 @@ const wrapperStyle = () => css`
   gap: 50px;
 `;
 
+const cardBoxStyle = () => css`
+  flex-direction: column;
+  flex-wrap: nowrap;
+`;
+
 const BoxStyle = () => css`
+  flex-direction: column;
   flex-grow: 1;
+  flex-wrap: nowrap;
 `;
 
 const Resume = () => {
   return (
     <div css={wrapperStyle()}>
-      <Card>테스트</Card>
-      <div css={BoxStyle()}>테스트111</div>
+      <div css={cardBoxStyle()}>
+        <Card>테스트</Card>
+      </div>
+      <div css={BoxStyle()}>
+        <Article name="Name" main="Main Title" sub="Sub Title" content="contentntasdfadsfsa" />
+      </div>
     </div>
   );
 };
