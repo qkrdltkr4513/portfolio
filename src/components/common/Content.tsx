eee0e44e17d03f3b22df2161e83efefdc88b06f7
austin <qkrdltkr4513@gmail.com>
2023-05-14 12:02:07 +0900

    [WORK-8][컴포넌트 생성] Content Component 생성
    - Content 컴포넌트 생성 및 스타일 반영
    - themes에 color, fontSize 추가 정의

---
+++ src/components/common/Content.tsx
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+import { CommonContentProps } from '@core/types';
+import { themes } from '@styles/themes';
+
+const wrapperStyle = () => css`
+  display: flex;
+  flex-direction: column;
+  color: ${themes.colors.Contents};
+  ${themes.fontSize.H1};
+  ${themes.fontWeight.Normal};
+`;
+
+const Content = ({ content }: CommonContentProps) => {
+  return <div css={wrapperStyle()}>{content}</div>;
+};
+
+export default Content;
