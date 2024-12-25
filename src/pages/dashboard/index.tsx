685700a7afbdd7a5a34956fd75a46101d70b54bd
Austin.park <34262987+qkrdltkr4513@users.noreply.github.com>
2024-12-25 15:47:31 +0900

    [WORK-34][개발] 대시보드 페이지 생성 (#58)
    [WORK-34][개발] 대시보드 페이지 생성
    
    - 대시보드 페이지 카드 컴포넌트를 통한 마크업 및 스타일링 추가
    - 공용 상수 내용 수정 및 순서 변경

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,25 @@
+import GridBox from '@components/common/GridBox';
+import GridCard from '@components/common/GridCard';
+
 const Dashboard = () => {
   return (
-    <div>
-      <div>프로젝트 누적 경험 횟수(라인 차트) / 프로젝트 경험 요약 (테이블) 4 : 2 영역 비율</div>
-      <div>
-        서비스별(웹, 모바일, 데스크탑) 경험 횟수 (파이 차트) / 프로젝트에 사용한 기술 경험 횟수 (바
-        차트) / 회사별 경력 및 총 경력 (도넛차트) 2 : 2 : 2 영역 비율
-      </div>
-    </div>
+    <GridBox>
+      <GridCard columnSize={'1 / 3'} height={400}>
+        프로젝트 누적 경험 횟수(라인 차트)
+      </GridCard>
+      <GridCard columnSize={'3 / 5'} height={400}>
+        프로젝트 경험 요약 (테이블){' '}
+      </GridCard>
+      <GridCard columnSize={'1 / 3'} height={400}>
+        프로젝트에 사용한 기술 경험 횟수 (바 차트)
+      </GridCard>
+      <GridCard columnSize={'3 / 4'} height={400}>
+        서비스별(웹, 모바일, 데스크탑) 경험 횟수 (파이 차트)
+      </GridCard>
+      <GridCard columnSize={'4 / 5'} height={400}>
+        회사별 경력 및 총 경력 (도넛차트)
+      </GridCard>
+    </GridBox>
   );
 };
 
