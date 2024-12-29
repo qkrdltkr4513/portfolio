b590dda86cb428651f267ff37ad3057b8f717785
austin <qkrdltkr4513@gmail.com>
2024-12-29 16:58:41 +0900

    [WORK-37][개발] Donut Chart 컴포넌트 생성
    - 대시보드에 표시 할 도넛 차트 생성
    - 도넛 스타일링 커스텀
    - type, config 구성

--- src/components/common/DonutChart.tsx
+++ src/components/common/DonutChart.tsx
@@ -9,64 +9,20 @@ const wrapperStyle = () => css`
   foreignObject {
     height: 100%;
   }
+
+  .apexcharts-datalabels-group {
+    text {
+      font-size: 16px;
+    }
+  }
+  .apexcharts-tooltip.apexcharts-theme-dark {
+    padding: 6px;
+    font-size: 14px;
+  }
 `;
 
-const DonutChart = (): JSX.Element => {
-  const [chartState, setState] = useState({
-    width: '100%',
-    height: '100%',
-    series: [24, 21, 18, 29],
-    options: {
-      title: {
-        text: '총 경력',
-        align: 'left',
-        style: {
-          color: 'white',
-          fontWeight: 500,
-        },
-      },
-      labels: ['atos', 'Build it', 'StageFive', 'NC ITS'],
-      legend: {
-        position: 'bottom',
-        labels: {
-          colors: ['white', 'white', 'white', 'white'],
-        },
-        onItemClick: {
-          toggleDataSeries: false,
-        },
-      },
-      tooltip: {
-        custom: (opt: any) => {
-          const { seriesIndex, w } = opt;
-          const series = w.config.series;
-          const labels = w.config.labels;
-
-          // TODO: 아래 툴팁 스타일 커스텀 필요
-          const result = `${labels[seriesIndex]}: ${series[seriesIndex]}개월`;
-
-          return result;
-        },
-      },
-      plotOptions: {
-        pie: {
-          donut: {
-            labels: {
-              show: true,
-              value: {
-                show: false,
-              },
-              total: {
-                show: true,
-                showAlways: true,
-                label: '총 경력 8년 6개월',
-                color: 'white',
-              },
-            },
-          },
-        },
-      },
-    },
-  });
+const DonutChart = (props: ChartProps): JSX.Element => {
+  const [chartState] = useState(props.config);
 
   return (
     <div css={wrapperStyle()}>
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,85 @@
+import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
+import { type Props as ChartProps } from 'react-apexcharts';
 import GridBox from '@components/common/GridBox';
 import GridCard from '@components/common/GridCard';
 
 const DonutChart = dynamic(() => import('@components/common/DonutChart'), { ssr: false });
 
+// 총 경력
+const DONUT_CHART_CONFIG = {
+  width: '100%',
+  height: '100%',
+  series: [24, 21, 18, 29],
+  options: {
+    title: {
+      text: '총 경력',
+      align: 'left',
+      style: {
+        color: 'white',
+        fontWeight: 500,
+      },
+    },
+    labels: ['atos', 'Build it', 'StageFive', 'NC ITS'],
+    legend: {
+      position: 'bottom',
+      labels: {
+        colors: ['white', 'white', 'white', 'white'],
+      },
+      onItemClick: {
+        toggleDataSeries: false,
+      },
+    },
+    tooltip: {
+      custom: (opt: any) => {
+        const { seriesIndex, w } = opt;
+        const series = w.config.series;
+        const labels = w.config.labels;
+
+        // TODO: 아래 툴팁 스타일 커스텀 필요
+        const result = `${labels[seriesIndex]}: ${series[seriesIndex]}개월`;
+        return result;
+      },
+    },
+    plotOptions: {
+      pie: {
+        // dataLabels: {
+        //   offset: 150,
+        // },
+        donut: {
+          labels: {
+            show: true,
+            value: {
+              show: false,
+            },
+            name: {
+              offsetY: 5,
+            },
+            total: {
+              show: true,
+              showAlways: true,
+              // label: '총 경력 8년 6개월',
+              label: '8년 6개월',
+              color: 'white',
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
 const Dashboard = () => {
+  // donut chart
+  const [donutChartConfig, setDonutChartConfig] = useState<ChartProps>();
+
+  // pie chart
+  const [pidChartConfig, setPieChartConfig] = useState<ChartProps>();
+
+  useEffect(() => {
+    setDonutChartConfig(DONUT_CHART_CONFIG as ChartProps);
+  }, []);
+
   return (
     <GridBox>
       <GridCard columnSize={'1 / 3'} height={400}>
@@ -21,7 +96,7 @@ const Dashboard = () => {
       </GridCard>
       <GridCard columnSize={'4 / 5'} height={400}>
         {/* 회사별 경력 및 총 경력 (도넛차트) */}
-        <DonutChart />
+        <DonutChart config={donutChartConfig} />
       </GridCard>
     </GridBox>
   );
