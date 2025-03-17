fd5e874e73aafe50dd6ebb7a99f6dcf107b40369
austin <qkrdltkr4513@gmail.com>
2025-03-17 19:15:13 +0900

    [WORK-48][개발] 대시보드 페이지의 차트 추가
    - 대시보드 페이지에 각각의 데이터를 표시하는 차트 추가
    - 차트 및 데이터에 따른 스타일 반영 및 수정

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,158 +2,143 @@ import { useEffect, useState } from 'react';
 import { themes } from '@styles/themes';
 import dynamic from 'next/dynamic';
 import { type Props as ChartProps } from 'react-apexcharts';
+import { TableProps } from 'antd';
 import GridBox from '@components/common/GridBox';
 import GridCard from '@components/common/GridCard';
+import Table from '@components/common/Table';
+
+import * as styles from './index.styles';
+
+import {
+  DONUT_CHART_CONFIG,
+  PIE_CHART_CONFIG,
+  RANGE_BAR_CHART_CONFIG,
+  TREEMAP_CHART_CONFIG,
+  TABLE_CONFIG,
+} from '@core/config';
 
 const DonutChart = dynamic(() => import('@components/common/DonutChart'), { ssr: false });
 const PieChart = dynamic(() => import('@components/common/PieChart'), { ssr: false });
-
-// 총 경력
-const DONUT_CHART_CONFIG = {
-  width: '100%',
-  height: '100%',
-  series: [24, 21, 18, 29],
-  options: {
-    title: {
-      text: '총 경력',
-      align: 'left',
-      style: {
-        color: 'white',
-        fontWeight: 500,
-      },
-    },
-    labels: ['atos', 'Build it', 'StageFive', 'NC ITS'],
-    legend: {
-      position: 'bottom',
-      labels: {
-        colors: ['white', 'white', 'white', 'white'],
-      },
-      onItemClick: {
-        toggleDataSeries: false,
-      },
-    },
-    tooltip: {
-      custom: (opt: any) => {
-        const { seriesIndex, w } = opt;
-        const series = w.config.series;
-        const labels = w.config.labels;
-
-        // TODO: 아래 툴팁 스타일 커스텀 필요
-        const result = `<div class='custom-tooltip'> ${labels[seriesIndex]}: ${series[seriesIndex]}개월</div>`;
-        return result;
-      },
-    },
-    plotOptions: {
-      pie: {
-        donut: {
-          labels: {
-            show: true,
-            value: {
-              show: false,
-            },
-            name: {
-              offsetY: 5,
-            },
-            total: {
-              show: true,
-              showAlways: true,
-              label: '8년 6개월',
-              color: 'white',
-            },
-          },
-        },
-      },
-    },
-  },
-};
-
-// 프로젝트 주 사용 기술
-const PIE_CHART_CONFIG = {
-  width: '100%',
-  height: '100%',
-  series: [3, 8, 1, 11, 6],
-  options: {
-    title: {
-      text: '프로젝트 주 사용 기술',
-      align: 'left',
-      style: {
-        color: 'white',
-        fontWeight: 500,
-      },
-    },
-    colors: [
-      themes.colors.Next,
-      themes.colors.React,
-      themes.colors.ReactNative,
-      themes.colors.Vue,
-      themes.colors.Typescript,
-    ],
-    labels: ['Next', 'React.js', 'React-Native', 'Vue.js (앱 포함)', 'Typescript'],
-    legend: {
-      position: 'bottom',
-      labels: {
-        colors: ['white', 'white', 'white', 'white', 'white'],
-      },
-      onItemClick: {
-        toggleDataSeries: false,
-      },
-    },
-    tooltip: {
-      custom: (opt: any) => {
-        const { seriesIndex, w } = opt;
-        const series = w.config.series;
-        const labels = w.config.labels;
-
-        // TODO: 아래 툴팁 스타일 커스텀 필요
-        const result = `<div class='custom-tooltip'> ${labels[seriesIndex]}: ${series[seriesIndex]}번</div>`;
-        return result;
-      },
-    },
-  },
-};
+const RangeBarChart = dynamic(() => import('@components/common/RangeBarChart'), { ssr: false });
+const TreeMapChart = dynamic(() => import('@components/common/TreeMapChart'), { ssr: false });
 
 const Dashboard = () => {
   // donut chart
   const [donutChartConfig, setDonutChartConfig] = useState<ChartProps>();
 
   // pie chart
-  const [pidChartConfig, setPieChartConfig] = useState<ChartProps>();
+  const [pieChartConfig, setPieChartConfig] = useState<ChartProps>();
+
+  // 경험 기술
+  const [treemapChartConfig, setTreemapChartConfig] = useState<ChartProps>();
+
+  // range bar chart
+  const [rangeBarChartConfig, setRangeBarChartConfig] = useState<ChartProps>();
+
+  // table
+  const [tableConfig, setTableConfig] = useState<TableProps>();
+
+  // 트리맵 차트의 레전드 클릭시 호출한다.
+  const onClickTreemapLegend = (index: number | 'ALL') => {
+    if (typeof index === 'number') {
+      setTreemapChartConfig((prev) => {
+        return { ...prev, series: [TREEMAP_CHART_CONFIG.series[index]] };
+      });
+    } else {
+      setTreemapChartConfig((prev) => {
+        return { ...prev, series: [...TREEMAP_CHART_CONFIG.series] };
+      });
+    }
+  };
 
   useEffect(() => {
+    setTreemapChartConfig(TREEMAP_CHART_CONFIG as ChartProps);
+    // setRangeBarChartConfig(RANGE_BAR_CHART_CONFIG as unknown as ChartProps);
+    setTimeout(() => {
+      setRangeBarChartConfig(RANGE_BAR_CHART_CONFIG as unknown as ChartProps);
+    }, 100);
     setDonutChartConfig(DONUT_CHART_CONFIG as ChartProps);
     setPieChartConfig(PIE_CHART_CONFIG as ChartProps);
+    setTableConfig(TABLE_CONFIG as unknown as TableProps);
   }, []);
 
   return (
-    <GridBox>
+    <GridBox css={styles.wrapperStyle()}>
       <GridCard columnSize={'1 / 3'} height={400}>
-        프로젝트 누적 경험 횟수(라인 차트)
+        {/* 연도별 프로젝트 사용기술 기간 */}
+        {rangeBarChartConfig && (
+          <RangeBarChart
+            width={rangeBarChartConfig?.width}
+            height={rangeBarChartConfig?.height}
+            series={rangeBarChartConfig.series}
+            options={rangeBarChartConfig.options}
+          />
+        )}
       </GridCard>
       <GridCard columnSize={'3 / 4'} height={400}>
-        MBTI를 근거로 주관적인 본인의 개발 성향 (레이더){' '}
+        {treemapChartConfig && (
+          <div css={styles.treemapChartBoxStyle()}>
+            <TreeMapChart
+              width={treemapChartConfig.width}
+              height={treemapChartConfig.height}
+              series={treemapChartConfig.series}
+              options={treemapChartConfig.options}
+            />
+            <div css={styles.treemapLegendBoxStyle()}>
+              <div key={`legend-item-all`} onClick={() => onClickTreemapLegend('ALL')}>
+                <span css={styles.legendCircleStyle(themes.colors.ChartPupple)} />
+                <span css={styles.treemapLegendTextStyle()}>전체</span>
+              </div>
+              {treemapChartConfig.customLegends.map(
+                (legend: { color: string; name: string }, index: number) => {
+                  const { color, name } = legend;
+                  return (
+                    <div key={`legend-item-${color}`} onClick={() => onClickTreemapLegend(index)}>
+                      <span css={styles.legendCircleStyle(color)} />
+                      <span css={styles.treemapLegendTextStyle()}>{name}</span>
+                    </div>
+                  );
+                },
+              )}
+            </div>
+          </div>
+        )}
       </GridCard>
       <GridCard columnSize={'4 / 5'} height={400}>
         {/* 서비스별(웹, 모바일, 데스크탑) 경험 횟수 (파이 차트) */}
-        {/* 플랫폼별 프로젝트 경험 횟수 (파이 차트) */}
-        <PieChart
-          width={pidChartConfig?.width}
-          height={pidChartConfig?.height}
-          series={pidChartConfig?.series}
-          options={pidChartConfig?.options}
-        />
+        {pieChartConfig && (
+          <div css={styles.customTooltipBoxStyle()}>
+            <PieChart
+              width={pieChartConfig?.width}
+              height={pieChartConfig?.height}
+              series={pieChartConfig?.series}
+              options={pieChartConfig?.options}
+            />
+          </div>
+        )}
       </GridCard>
       <GridCard columnSize={'1 / 4'} height={400}>
-        {/* 프로젝트에 사용한 기술 경험 횟수 (바 차트) */}
-        프로젝트 경험 요약 (테이블){' '}
+        {/* 프로젝트 경험 요약 (테이블){' '} */}
+        {tableConfig && (
+          <Table
+            title={tableConfig.title}
+            columns={tableConfig.columns}
+            dataSource={tableConfig.dataSource}
+            pagination={tableConfig.pagination as false}
+            scroll={tableConfig.scroll}
+          />
+        )}
       </GridCard>
       <GridCard columnSize={'4 / 5'} height={400}>
         {/* 회사별 경력 및 총 경력 (도넛차트) */}
-        <DonutChart
-          width={donutChartConfig?.width}
-          height={donutChartConfig?.height}
-          series={donutChartConfig?.series}
-          options={donutChartConfig?.options}
-        />
+        {donutChartConfig && (
+          <DonutChart
+            width={donutChartConfig?.width}
+            height={donutChartConfig?.height}
+            series={donutChartConfig?.series}
+            options={donutChartConfig?.options}
+          />
+        )}
       </GridCard>
     </GridBox>
   );
