bf81c673ea8907e7f30272f956847b5f2ef060f4
Austin.park <34262987+qkrdltkr4513@users.noreply.github.com>
2025-03-17 19:13:30 +0900

    [WORK-40][개발] Grid 컴포넌트 생성 (#71)
    [WORK-40][개발] Grid 컴포넌트 생성
    
    - antd 테이블을 통한 공용 테이블 컴포넌트 생성
    - 공용 테마에 컬러 추가
    - antd 라이브러리 설치

---
+++ src/components/common/Table.tsx
@@ -0,0 +1,33 @@
+import { Table as AntdTable, Tag, ConfigProvider, TableProps } from 'antd';
+import { themes } from '@styles/themes';
+
+const Table = ({
+  title,
+  columns,
+  dataSource,
+  scroll,
+  pagination = false,
+  ...restPorps
+}: TableProps) => {
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorBgContainer: `${themes.colors.CardBackGround}`,
+          colorText: `${themes.colors.White}`,
+        },
+      }}
+    >
+      <AntdTable
+        title={title}
+        dataSource={dataSource}
+        columns={columns}
+        pagination={pagination}
+        scroll={scroll}
+        {...restPorps}
+      />
+    </ConfigProvider>
+  );
+};
+
+export default Table;
