cb589b87f3c7f1a6fef242d5292bf60c6b56590f
Austin.park <34262987+qkrdltkr4513@users.noreply.github.com>
2025-03-17 19:11:24 +0900

    [WORK-46][개발] Treemap 컴포넌트 차트 생성  (#70)
    [WORK-46][개발] Treemap 컴포넌트 차트 생성
    
    - Treemap 차트 컴포넌트 생성
    - 공용 Column 차트 생성

---
+++ src/components/common/ColumnChart.tsx
@@ -0,0 +1,29 @@
+import Chart, { type Props as ChartProps } from 'react-apexcharts';
+import { css } from '@emotion/react';
+// import dynamic from 'next/dynamic';
+
+// const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const wrapperStyle = () => css`
+  width: 100%;
+  height: 100%;
+`;
+
+const ColumnChart = ({ width, height, series, options, ...restProps }: ChartProps): JSX.Element => {
+  return (
+    <div css={wrapperStyle()}>
+      <Chart
+        type="bar"
+        width={width}
+        height={height}
+        series={series}
+        options={(options as ChartProps['options']) || {}}
+        stacked={true}
+        stackType={'100%'}
+        {...restProps}
+      />
+    </div>
+  );
+};
+
+export default ColumnChart;
