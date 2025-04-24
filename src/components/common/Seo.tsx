785a2da2932fdb7ee558775aeee853a59e2aba72
austin <qkrdltkr4513@gmail.com>
2025-04-25 02:11:12 +0900

    no message

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -8,18 +8,21 @@ interface SeoProps {
 }
 
 const Seo = ({ title, description, url, imageName }: SeoProps) => {
+  console.log('imageName');
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <link rel="canonical" href={url} />
+      <link rel="canonical" href={`/portfolio${url}`} />
 
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:url" content={url} />
+      <meta property="og:url" content={`/portfolio${url}`} />
       <meta property="og:site_name" content="[포트폴리오] FE Developer - 박이삭" />
-      {imageName && <meta property="og:image" content={`${imageName}`} />}
+      {imageName && (
+        <meta property="og:image" content={`https://qkrdltkr4513.github.io${imageName}`} />
+      )}
 
       {/* <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
