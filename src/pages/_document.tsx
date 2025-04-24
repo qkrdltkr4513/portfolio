2ea0e9ce84181bd6035d576e2174134bb43e45f2
austin <qkrdltkr4513@gmail.com>
2025-04-25 01:44:01 +0900

    [main] favicon 경로 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/portfolio/favicon.ico" />
         </Head>
         <body>
           <Main />
