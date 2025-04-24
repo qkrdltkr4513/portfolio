39468bdfe3ecb279c59cc256a7197b9eef180e91
austin <qkrdltkr4513@gmail.com>
2025-04-25 00:18:44 +0900

    [main] next.config 설정 오타 수정

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfoilo' : '',
-  assetPrefix: isProd ? '/portfoilo/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
