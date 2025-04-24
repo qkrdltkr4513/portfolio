1f0ce8cea9991a50693d314663f8d35d85c1a309
austin <qkrdltkr4513@gmail.com>
2025-04-25 01:49:39 +0900

    [main] 오픈그래프 이미지 경로 추가

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -19,7 +19,9 @@ const Seo = ({ title, description, url, imageName }: SeoProps) => {
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
       <meta property="og:site_name" content="[포트폴리오] FE Developer - 박이삭" />
-      {imageName && <meta property="og:image" content={imageName} />}
+      {imageName && (
+        <meta property="og:image" content={`https://qkrdltkr4513.github.io${imageName}`} />
+      )}
 
       {/* <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
